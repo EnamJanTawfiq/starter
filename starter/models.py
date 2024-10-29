ceb6628dbc03c6212a14594e5f6d5315446fbdff
EnamJanTawfiq <enamjantawfiq@gmail.com>
2024-10-29 14:18:06 +0430

    change

--- starter/models.py
+++ starter/models.py
@@ -5,6 +5,12 @@ class Product(models.Model):
     description=models.TextField()
     price=models.IntegerField()
     image=models.ImageField(upload_to='images/',null=True)
+
+class User(models.Model):
+    title=models.CharField(max_length=255)
+    description=models.TextField()
+    price=models.IntegerField()
+    image=models.ImageField(upload_to='images/',null=True)
     
 
     
